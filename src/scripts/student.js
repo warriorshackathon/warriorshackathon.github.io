90954994ec88fd280bed0225e983a9d7e601ac5d
Keith Cheung <kcheung@l003816.local>
2017-07-12 10:20:33 -0600

    initialize new repo

---
+++ src/scripts/student.js
@@ -0,0 +1,250 @@
+var drawPositions = [];
+var type = 'Ink';
+var InkColor = 'black';
+var InkWidth = 5;
+
+var background = document.getElementById("background");
+var bgd = background.getContext("2d");
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+
+var studentId = '';
+var studentName = 'Defaulto';
+
+main();
+
+function main() {
+
+    var config = {
+        apiKey: "AIzaSyCJd8QeVygCdNRsURjM-pB-MIfaq7itALs",
+        authDomain: "warriors-8d7c1.firebaseapp.com",
+        databaseURL: "https://warriors-8d7c1.firebaseio.com",
+        projectId: "warriors-8d7c1",
+        storageBucket: "",
+        messagingSenderId: "860681092403"
+    };
+
+    firebase.initializeApp(config);
+
+    firebase.database().ref('/12345/').on('value', function(snapshot) {
+
+        console.log(snapshot.val());
+        if(snapshot.val()) {
+        if (snapshot.val().teacherbase64) {
+            file = snapshot.val().teacherbase64;
+            $('#source').hide();
+            $('#canvasId').show();
+            if (file == '') {
+                setCanvas(500, 500);
+            } else if (file.includes('.pdf')) {
+                convertToPDF(file, 1)
+            } else {
+                bg = new Image();
+                bg.crossOrigin = "Anonymous";
+                bg.src = file;
+                setCanvas(500, 500);
+                bg.onload = function() {
+                    bgd.drawImage(bg, 0, 0, 500, 500);
+                }
+            }
+
+            document.querySelectorAll('button').forEach(function(button) {
+                button.addEventListener('click', function(e) {
+                    if (e.target.parentNode.parentNode.id == 'types' && e.target.id != 'Finish') {
+                        if (type=='Highlight'){
+                            changeColor(color);
+                            ctx.globalAlpha = 1;
+                        }
+                        type = e.target.innerText;
+                    }
+                    else if(e.target.id == 'Finish'){
+                        combineCanvases();
+                    }
+                    else if(e.target.parentNode.id == 'colors'){
+
+                    }
+                    else if(e.target.parentNode.id == 'width'){
+
+                    }
+                });
+
+
+                canvas.addEventListener('mousedown', function(e) {
+                    if (type == 'Ink') {
+                        setDefault(InkColor,InkWidth);
+                        updateAndDraw(e);
+                        canvas.onmousemove = function(e) {
+                            updateAndDraw(e);
+                        }
+                    } else if (type == 'Erase'){
+                        eraseContent(e);
+                        canvas.onmousemove = function(e) {
+                            eraseContent(e);
+                        }
+                    }
+                    else if (type == 'Highlight'){
+                        highlight(e);
+                        canvas.onmousemove = function(e) {
+                            highlight(e);
+                        }
+                    }
+                });
+
+                canvas.addEventListener('touchstart', function(e) {
+                    e.preventDefault();
+                    if (type == 'Ink') {
+                        updateAndDraw(e);
+                        canvas.ontouchmove = function(e) {
+                            e.preventDefault();
+                            updateAndDraw(e);
+                        }
+                    } else if (type == 'Erase'){
+                        eraseContent(e);
+                        canvas.ontouchmove = function(e) {
+                            e.preventDefault();
+                            eraseContent(e);
+                        }
+                    }
+                    else if (type == 'Highlight'){
+                        highlight(e);
+                        canvas.ontouchmove = function(e) {
+                            e.preventDefault();
+                            highlight(e);
+                        }
+                    }
+                });
+
+                canvas.addEventListener("mouseup", function(e) {
+                    if (type == 'Ink') {
+                        canvas.onmousemove = function(e) {
+                            drawPositions = [];
+                        }
+                    } else {
+                        canvas.onmousemove = null
+                    }
+                });
+
+                canvas.addEventListener("touchend", function(e) {
+                    if (type == 'Ink') {
+                        canvas.ontouchmove = function(e) {
+                            drawPositions = [];
+                        }
+                    } else {
+                        canvas.ontouchmove = null
+                    }
+                });
+
+                canvas.addEventListener('click', function(e){
+                    if(type == 'Text'){
+                        addText(e,'text would go here');
+                    }
+                });
+            })
+        }
+        else {
+            $('#source').show();
+            $('#canvasId').hide();
+            
+        }
+    }
+    else {
+        $('#source').show();
+        $('#canvasId').hide();
+
+    }
+    })
+}
+
+function combineCanvases(){
+    bgd.drawImage(canvas,0,0);
+    var dataURL = background.toDataURL('jpg');
+    if (studentId) {
+        firebase.database().ref('/12345/studentList/'+studentId.path.o[2]).set({studentImg: dataURL, name: studentName})
+    } else {
+        studentId = firebase.database().ref('/12345/studentList/').push({studentImg: dataURL, name: studentName})
+    }
+}
+
+function setDefault(color, width){
+    changeColor(color);
+    changeWidth(width);
+}
+function setCanvas(height, width) {
+    fileheight= height;
+    filewidth = width;
+    canvas.height = height;
+    canvas.width = width;
+    background.height = height;
+    background.width = width;
+    document.getElementById('canvasId').style.height=height+'px';
+    document.getElementById('canvasId').style.width=width+'px';
+}
+
+function updateAndDraw(e) {
+    drawPositions.push([e.pageX, e.pageY]);
+    drawLine();
+}
+
+function drawLine() {
+    if (drawPositions.length >= 2) {
+        var start = drawPositions.shift();
+        var end = drawPositions[0];
+        ctx.beginPath();
+        ctx.moveTo(start[0], start[1]);
+        ctx.lineTo(end[0], end[1]);
+        ctx.closePath();
+        ctx.stroke();
+    }
+}
+
+function addText(pointer, text){
+    var x = pointer.pageX;
+    var y = pointer.pageY;
+    ctx.fillText(text,x,y);
+}
+
+function highlight(pointer){
+    var x = pointer.pageX;
+    var y = pointer.pageY;
+    ctx.globalAlpha = 0.1;
+    ctx.fillStyle = "yellow";
+    ctx.fillRect(x - 5, y - 5, 10, 10);
+}
+
+function changeColor(color){
+    ctx.strokeStyle=color;
+    ctx.fillStyle=color;
+}
+
+function changeWidth(width){
+    ctx.lineWidth=width;
+}
+function eraseContent(pointer) {
+    var x = pointer.pageX;
+    var y = pointer.pageY;
+    ctx.clearRect(x - 5, y - 5, 10, 10);
+}
+
+function convertToPDF(pdfurl, pageNum) {
+    var pdf = new PDFJS.getDocument(pdfurl);
+    PDFJS.getDocument(pdfurl).then(function(pdf) {
+        pdf.getPage(pageNum).then(function(page) {
+            var scale = 1;
+            var viewport = page.getViewport(scale);
+            var viewportHeight = viewport.height * scale;
+            var viewportWidth = viewport.width * scale;
+            setCanvas(viewportHeight, viewportWidth);
+            var renderContext = {
+                canvasContext: bgd,
+                viewport: viewport
+            };
+            var renderTask = page.render(renderContext);
+            renderTask.then(function() {
+            });
+        });
+    });
+}
+
+function addName() {
+    studentName = document.getElementById("nameInput").value
+}
---
+++ src/scripts/teacher.js
@@ -0,0 +1,136 @@
+var teacherImg;
+var sessionId;
+
+$(document).ready(function() {
+
+  $('#teacherReviewSessionName').html('Session ID: ' + sessionId);
+  
+  $('.list-group-item').click(function() {
+    $('.list-group-item .list-group-item-inner').css({'display': 'none'});
+    $(this).find('.list-group-item-inner').css({'display': 'block'});
+
+    $('.list-group-item .badge').css({'display': 'block'});
+    $(this).find('.badge').css({'display': 'none'});
+  });
+
+});
+
+function updateImageFromTemplate(imgPath) {
+  toDataUrl(imgPath, function(myBase64) {
+      updateTeacherImg(myBase64);
+  });
+}
+
+function toDataUrl(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.onload = function() {
+        var reader = new FileReader();
+        reader.onloadend = function() {
+            callback(reader.result);
+        }
+        reader.readAsDataURL(xhr.response);
+    };
+    xhr.open('GET', url);
+    xhr.responseType = 'blob';
+    xhr.send();
+}
+
+function readURL(input) {
+  if (input.files && input.files[0]) {
+      var reader = new FileReader();
+
+      reader.onload = function(e) {
+        updateTeacherImg(e.target.result);
+      }
+
+      reader.readAsDataURL(input.files[0]);
+  }
+}
+
+function updateTeacherImg(src) {
+  // this looks like redundant code but it isn't! (Bad variable names!!)
+  // teacherImg is the base64 string, #teacherImg is the html element
+  teacherImg = src;
+  $('#teacherImg').attr('src', src);
+  $('#sendImageButton').removeClass('disabled');
+}
+
+function downloadPDF() {
+  var name;
+  var imgsrc;
+  var childrenElement;
+  var reviewElements = $('.review')
+  var reviewLength = reviewElements.length;
+  var doc = new jsPDF();
+  doc.setFontSize(40);
+  for (i = 0; i < reviewLength; ++i) {
+    childrenElement = reviewElements[i].children;
+    imgsrc = childrenElement[0].src;
+    name = childrenElement[1].innerText;
+    doc.addImage(imgsrc, 'JPEG', 15, 30, 175, 200);
+    doc.text(35, 260, name);
+    if (!(i == (reviewLength - 1))) {
+      doc.addPage();
+    }
+  }
+  doc.save('sample.pdf');
+}
+
+function resetActivity() {
+  console.log('reset');
+  sessionId = $("#sessionId").val();
+  firebase.database().ref('12345/studentList').off();
+  firebase.database().ref('12345/studentList').remove();
+  firebase.database().ref('12345').remove();
+  $('#teacher-review').hide();
+  $('#teacher-main').show();
+  var reviewElements = $('.review');
+  reviewElements.remove();
+  var rem = document.querySelectorAll("[id*='myModal']"), i = 0;
+  for (; i < rem.length; i++)
+    rem[i].parentNode.removeChild(rem[i]);
+  updateImageFromTemplate('resources/uploadImage.jpg');
+}
+
+function startSession() {
+
+  if (!teacherImg) {
+    alert("Button is disabled until an image is uploaded. To sent out a blank image, navigate to the 'General' template tab and select the first template");
+    return;
+  }
+
+  var lessonCount = 0;
+  try {
+  	sessionId = $("#sessionId").val();
+      var config = {
+        apiKey: "AIzaSyCJd8QeVygCdNRsURjM-pB-MIfaq7itALs",
+        authDomain: "warriors-8d7c1.firebaseapp.com",
+        databaseURL: "https://warriors-8d7c1.firebaseio.com",
+        projectId: "warriors-8d7c1",
+        storageBucket: "",
+        messagingSenderId: "860681092403"
+      };
+      firebase.initializeApp(config);
+  }
+  catch(err) {
+    console.log('Firebase already init');
+  }
+  if (teacherImg) {
+    firebase.database().ref('12345').set({teacherbase64: teacherImg});
+    firebase.database().ref('12345/studentList').on("child_added", function(snapshot, prevChildKey) {
+        $('#teacher-review').show();
+        $('#teacher-main').hide();
+        $('#lessonCount').html('Submissions: '+ (lessonCount + 1));
+        var image = new Image();
+        image.src = snapshot.val().studentImg;
+
+        var modal = '<div id="' + lessonCount + 'myModal" class="modal fade" role="dialog"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal">&times;</button><h4 class="modal-title">'+ snapshot.val().name +'</h4></div><div class="modal-body">'+ image.outerHTML +'</div><div class="modal-footer"><button type="button" class="btn btn-default" data-dismiss="modal">Close</button></div></div></div></div>'
+
+        var newDiv = "<div class='review' data-toggle='modal' data-target = '#" + lessonCount + "myModal'>" + image.outerHTML + "<p class='text-review'>" + snapshot.val().name + "</p></div>"
+        lessonCount++;
+        $('#teacher-review').append(modal);
+        $('#reviewElements').append(newDiv);
+
+      });
+    }
+}
