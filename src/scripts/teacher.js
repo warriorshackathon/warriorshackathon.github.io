0027bc8ab9ccb9ee84c41b51803bf18867510998
Liyang Wang <lwang@smarttech.com>
2017-07-12 15:37:31 -0600

    changing session id to be just numbers similar to Classlab

--- src/scripts/teacher.js
+++ src/scripts/teacher.js
@@ -99,9 +99,9 @@ function resetActivity() {
 
 function makeid() {
   var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var possible = "0123456789";
 
-  for (var i = 0; i < 5; i++)
+  for (var i = 0; i < 8; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
